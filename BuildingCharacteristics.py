c586e0de4a25a3ddc01a6edb561f34233e0d07ca
gerikcha <93253002+gerikcha@users.noreply.github.com>
2021-10-28 09:48:49 +0100

    Add files via upload
    Building characteristics file

--- BuildingCharacteristics.py
+++ BuildingCharacteristics.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 """
 Created on Thur Oct 28 2021 09:58:49 2021
 
@@ -19,3 +20,23 @@ def building_characteristics():
     bc = pd.read_excel(r'Building Characteristics.xlsx')
 >>>>>>> ea31618 (input file)
     return bc
+=======
+"""
+Created on Thur Oct 28 2021 09:58:49 2021
+
+@author: c.gerike-roberts
+
+This code is designed to read an excel file which contains the characteristics of the building
+and create a data frame from it.
+"""
+
+import pandas as pd
+
+
+# import excel file
+
+def BuildingCharacteristics():
+    bc = pd.read_excel(r'Building Characteristics.xlsx')
+    print(bc)
+    return bc
+>>>>>>> eed3252 (Add files via upload)
