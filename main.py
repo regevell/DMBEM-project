7e16b1d04a0abec38adeb8407503ddeb9979951d
gerikcha <gerikcha@students.zhaw.ch>
2021-10-28 09:55:57 +0100

    updated main.py

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
-print('this is the main')
-print('this is also the main')
+"""
+Started on 28 October 2021.
+
+Authors: L.Beber, E.Regev, C.Gerike-Roberts
+
+Code which models the dynamic thermal transfer in a building.
+"""
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import dm4bem
+import BuildingCharacteristics
+
+# Define building characteristics
+
+bc = BuildingCharacteristics.BuildingCharacteristics()
+print(bc)
+
+# Define thermal properties
