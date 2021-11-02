7a14e5ed9b81fb59a9d2b0d5d0c2d31069fada41
beberlen <beberlen@students.zhaw.ch>
2021-11-02 07:55:13 +0100

    Working solid_wall_w_ins.py and updated main.py and thermophysicalprop.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ bc = BuildingCharacteristics.building_characteristics()
 Kp = 1e4  # factor for HVAC
 dt = 5  # s - time step for solver
 T_set = pd.DataFrame([{'cooling': (26 + 273.15), 'heating': (22 + 273.15)}])  # K - temperature set points
-mesh = 1
+#mesh = 1
 Tm = 20 + 273.15  # K - Mean temperature for radiative exchange
 ACH = 1  # h*-1 - no. of air changes in volume per hour
 h = pd.DataFrame([{'in': 4., 'out': 10}])  # W/m² K - convection coefficients
@@ -37,17 +37,17 @@ Vdot = bc.Volume[4] * ACH / 3600  # m3/s - volume flow rate due to air changes
 bcp = thermophysicalprop.thphprop(bc)
 
 # Thermal Circuits
-TCd = np.zeros(len(bcp))
+TCd = {}
 
 for i in range(0, len(bcp)):
     if bcp.Element_Type[i] == 'Solid Wall w/In':
-        TCd[i] = solid_wall_w_ins.solid_wall_w_ins(bcp.loc[i, :], h)
+        TCd.update({'TCd'+str(i): solid_wall_w_ins.solid_wall_w_ins(bcp.loc[i, :], h)})
     # elif bcp.Element_Type[i] == 'DG':
-    #     TCd[i] = DG(bcp.loc[i, :], h)
+    #     TCd.update({'TCd'+str(i): DG(bcp.loc[i, :], h)})
     # elif bcp.Element_Type[i] == 'Suspended Floor':
-    #     TCd[i] = susp_floor(bcp.loc[i, :], h)
+    #     TCd.update({'TCd'+str(i): susp_floor(bcp.loc[i, :], h)})
     # elif bcp.Element_Type[i] == 'Flat Roof 1/In':
-    #     TCd[i] = flat_roof_w_in(bcp.loc[i, :], h)
+    #     TCd.update({'TCd'+str(i): flat_roof_w_in(bcp.loc[i, :], h)})
 
 print(TCd)
 # Define weather
--- solid_wall_w_ins.py
+++ solid_wall_w_ins.py
@@ -1,42 +1,42 @@
 """
 Created on Fr Oct 29 2021 14:50:47 2021
 
-@author: c.gerike-roberts
+@author: l.beber
 
-Input: bcp, h
-Output: TCd0
+Input:
+    bcp_r, one row of the bcp dataframe
+    h, convection dataframe
+Output: TCd, a dictionary of the all the matrices of one thermal circuit describing a solid wall with insulation
 """
 import numpy as np
-import pandas as pd
-
 
 def solid_wall_w_ins(bcp_r, h):
     # Thermal conductances
     # Conduction
-    G_cd_cm = bcp_r[['conductivity_1']] / bcp_r[['Thickness_1']] * bcp_r[['Surface']]  # concrete
-    G_cd_in = bcp_r[['conductivity_2']] / bcp_r[['Thickness_2']] * bcp_r[['Surface']]  # insulation
+    G_cd_cm = bcp_r['conductivity_1'] / bcp_r['Thickness_1'] * bcp_r['Surface']  # concrete
+    G_cd_in = bcp_r['conductivity_2'] / bcp_r['Thickness_2'] * bcp_r['Surface']  # insulation
 
     # Convection
-    Gw = h * bcp_r[['Surface']]  # wall
+    Gw = h * bcp_r['Surface']  # wall
 
     # Thermal capacities
-    Capacity_cm = bcp_r[['density_1']] * bcp_r[['specific_heat_1']] * bcp_r[['Surface']] * bcp_r[['Thickness_1']]
-    Capacity_in = bcp_r[['density_2']] * bcp_r[['specific_heat_2']] * bcp_r[['Surface']] * bcp_r[['Thickness_2']]
+    Capacity_cm = bcp_r['density_1'] * bcp_r['specific_heat_1'] * bcp_r['Surface'] * bcp_r['Thickness_1']
+    Capacity_in = bcp_r['density_2'] * bcp_r['specific_heat_2'] * bcp_r['Surface'] * bcp_r['Thickness_2']
 
     # Thermal network
     # ---------------
-    nq = 1 + 2 * (int(bcp_r[['Mesh_1']]) + int(bcp_r[['Mesh_2']]))
-    nt = 1 + 2 * (int(bcp_r[['Mesh_1']]) + int(bcp_r[['Mesh_2']]))
+    nq = 1 + 2 * (int(bcp_r['Mesh_1']) + int(bcp_r['Mesh_2']))
+    nt = 1 + 2 * (int(bcp_r['Mesh_1']) + int(bcp_r['Mesh_2']))
 
     A = np.eye(nq + 1, nt)
     A = -np.diff(A, 1, 0).T
 
-    nc = int(bcp_r[['Mesh_1']])
-    ni = int(bcp_r[['Mesh_2']])
+    nc = int(bcp_r['Mesh_1'])
+    ni = int(bcp_r['Mesh_2'])
     Gcm = 2 * nc * G_cd_cm
     Gcm = 2 * nc * np.array(Gcm)
     Gim = 2 * ni * G_cd_in
-    Gim = 2 * int(bcp_r[['Mesh_2']]) * np.array(Gim)
+    Gim = 2 * int(bcp_r['Mesh_2']) * np.array(Gim)
     G = np.diag(np.hstack([Gw['out'], Gcm, Gim]))
 
     b = np.zeros(nq)
